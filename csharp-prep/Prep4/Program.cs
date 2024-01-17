908bf93ac102f191b343c7bf2715a5e060d553e5
student-i <ladyannamarie@gmail.com>
2024-01-16 19:49:13 -0700

    Finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> numbers = new List<int>();
+
+        int addNumber = 3;
+
+        while (addNumber != 0){
+
+        Console.Write("What number do you want to add? ");
+        addNumber = int.Parse(Console.ReadLine());
+        
+        if (addNumber != 0){
+            numbers.Add(addNumber);
+        }
+
     }
+
+    int total = 0;
+    int biggest = 0;
+    int smallestPositive = 1000000000;
+
+    foreach(int number in numbers){
+        total += number;
+        if (number > biggest){
+            biggest = number;
+        }
+        if (number < smallestPositive && number > 0){
+            smallestPositive = number;
+        }
+        
+    }
+
+    Console.WriteLine($"Sum: {total}");
+    Console.WriteLine($"Average: {total/numbers.Count}");
+    Console.WriteLine($"Biggest number: {biggest}");
+    Console.WriteLine($"Smallest positive number: {smallestPositive}");
+    
+        }
 }
